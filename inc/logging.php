dfc0a9e8f3eaa56a8b903f6f3d35b6727190a894
Seth Gitner <smgitner@syr.edu>
2025-09-20 22:31:18 -0400

    Enable logging function for debugging block registration

--- inc/logging.php
+++ inc/logging.php
@@ -5,6 +5,10 @@ defined('ABSPATH') || exit;
  * Simple logging function to prevent errors
  */
 function nppf_log($message, $context = array(), $level = 'info') {
-    // Do nothing - just prevent the undefined function error
-    return;
+    // Enable logging for debugging
+    if (is_array($context) && !empty($context)) {
+        error_log('NPPF: ' . $message . ' | Context: ' . json_encode($context));
+    } else {
+        error_log('NPPF: ' . $message);
+    }
 }
