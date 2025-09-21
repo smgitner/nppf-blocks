461bc89570dbe8007a6994db8599d5424d259a9c
Seth Gitner <smgitner@syr.edu>
2025-09-20 22:15:03 -0400

    Add deprecated blocks to prevent site crashes - scholarship-grid and what-we-do now show deprecation notices

---
+++ blocks/scholarship-grid/render.php
@@ -0,0 +1,8 @@
+<?php
+defined('ABSPATH') || exit;
+?>
+
+<div class="nppf-deprecated-block" style="padding: 2rem; background-color: #f0f0f0; border: 2px dashed #ccc; text-align: center;">
+    <h3><?php _e('This block has been deprecated', 'nppf-blocks'); ?></h3>
+    <p><?php _e('Please replace this block with a supported block.', 'nppf-blocks'); ?></p>
+</div>
